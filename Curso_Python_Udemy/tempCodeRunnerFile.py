e1811e6c6507b306ba7c6ff5183029da75a0e7d7
Flávio Bueno <37403746+flaviobueno86@users.noreply.github.com>
2023-02-14 12:08:03 -0300

    Add files via upload
    Atualização curso

--- Curso_Python_Udemy/tempCodeRunnerFile.py
+++ Curso_Python_Udemy/tempCodeRunnerFile.py
@@ -1,15 +1,2 @@
-
-string = 'Valor qualquer'
-
-i = 0
-while i < len(string):
-    letra = string[i]
-
-    if letra == ' ':
-        break
-
-    print(letra)
-    i += 1
-else:
-    print('Não encontrei um espaço na string.')
-print('Fora do while.')
+for item in lista_enumerada:
+    print(item)
