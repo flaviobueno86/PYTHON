b82b3f40f46223aea8d15544d0d1ed7fba1e62a3
Flávio Bueno <37403746+flaviobueno86@users.noreply.github.com>
2023-03-10 14:45:02 -0300

    Add files via upload

---
+++ Curso_Python_Udemy/Sessão4 - Intermediario/aula145_iterables_iterators.py
@@ -0,0 +1,3 @@
+#  Generator expression, Iterables e Iterators em python
+iterables = ['Eu', 'Tenho', '__iter__']
+iterator = iterables.__iter__()  # tem __iter__e__next__
--- Curso_Python_Udemy/Sessão4 - Intermediario/tempCodeRunnerFile.py
+++ Curso_Python_Udemy/Sessão4 - Intermediario/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-elevado
+
+print(novos_nomes)
